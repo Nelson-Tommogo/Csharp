dfe0b31557ddd8e3883546fc96f59f7cd09d78c2
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 20:57:36 +0300

    C#

--- inheritance.cs
+++ inheritance.cs
@@ -1,12 +1,67 @@
 using System;
-namespace inherit{
-    public class animals{
-        public string name="animals";
-        public string breath="oxygen";
-        public bool respiration= true
 
+// Base class: Animal
+class Animal
+{
+    public string Species { get; set; }
+    public int Age { get; set; }
+
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating.");
     }
-    public class human:animals{
-        
+
+    public void Sleep()
+    {
+        Console.WriteLine("Animal is sleeping.");
+    }
+}
+
+// Derived class: Human, inheriting from Animal
+class Human : Animal
+{
+    public string Name { get; set; }
+    public string Gender { get; set; }
+
+    public void Talk()
+    {
+        Console.WriteLine("Hello, I am a human.");
+    }
+
+    public void Work()
+    {
+        Console.WriteLine("Human is working.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an instance of Animal
+        Animal animal = new Animal
+        {
+            Species = "Lion",
+            Age = 5
+        };
+
+        // Creating an instance of Human
+        Human human = new Human
+        {
+            Species = "Homo sapiens",
+            Age = 25,
+            Name = "John",
+            Gender = "Male"
+        };
+
+        // Calling methods of Animal class
+        animal.Eat();
+        animal.Sleep();
+
+        // Calling methods of Human class
+        human.Eat();
+        human.Sleep();
+        human.Talk();
+        human.Work();
     }
 }
