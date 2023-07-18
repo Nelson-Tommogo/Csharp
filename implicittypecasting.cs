59b75ea6c6b97f0c25f99a70052d6f88b895f414
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 14:56:14 +0300

    C#

--- implicittypecasting.cs
+++ implicittypecasting.cs
@@ -2,7 +2,10 @@ using System;
 namespace typecasting{
     public class typecast{
         public static void Main(){
-            
+            int AppProgarmming = 71;
+            double AdvancedApp = AppProgarmming;
+            Console.WriteLine(AppProgarmming);
+            Console.WriteLine(AdvancedApp);
         }
     }
 }
