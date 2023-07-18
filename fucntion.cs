49d4ddc4c9dbf2b47a9d6833e439efdb6b6528f2
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 17:00:28 +0300

    C#

--- fucntion.cs
+++ fucntion.cs
@@ -5,8 +5,23 @@ public class Functions{
         Console.WriteLine("Let's Learn Something new today");
     }
     static void SemesterGPA(){
+        int unit1 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit1");
-        Console.readLine();
+        int unit2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit2");
+        int unit3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit3");
+        int unit3 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit4");
+        int unit4 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit5");
+        int unit5 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit6");
+        int Unit6 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit7");
+        int unit7 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit8");
+        int unit8 = int.Parse(Console.ReadLine());
     }
     public static void Main(){
         Console.WriteLine("This is the Function");
