510e873bd3869caf911300c6e6d4ba6ac2d91292
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 16:53:24 +0300

    C#

--- fucntion.cs
+++ fucntion.cs
@@ -1,7 +1,16 @@
 using System;
 namespace Func{
 public class Functions{
-    public static void Main{}
-
-    
+    static void greeting(){
+        Console.WriteLine("Let's Learn Something new today");
+    }
+    static void SemesterGPA(){
+        Console.WriteLine("Enter Marks of Unit1");
+        Console.readLine();
+    }
+    public static void Main(){
+        Console.WriteLine("This is the Function");
+        greeting();
+        SemesterGPA();
+    }    
 }}
