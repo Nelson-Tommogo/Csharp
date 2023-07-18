6818ab12c60be53b1b492314049344805a235c78
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 20:22:03 +0300

    C#

--- fucntion.cs
+++ fucntion.cs
@@ -46,7 +46,24 @@ namespace Func
             Double Average = total / 9;
             Double GPA =Average/2;
 
-            Console.WriteLine($"Total Marks: {total}, Average: {Average}, GPA: {GPA}");
+            string grade = "";
+            if(Average <= 30 && Average >0){
+                Console.WriteLine("D");
+            }
+            else if(Average<=40 && Average>= 31){
+            Console.WriteLine("C");
+            }
+             else if(Average<=55 && Average>= 41){
+            Console.WriteLine("B-");
+            }
+             else if(Average<=65 && Average>= 56){
+            Console.WriteLine("B");
+            }
+             else if(Average<=74 && Average>= 66){
+            Console.WriteLine("B+");
+            }
+
+            Console.WriteLine($"Total Marks: {total}, Average: {Average}, GPA: {GPA}, grade:{grade}");
         }
 
         public static void Main()
