a7a4ee196f47b78d4d7e5404d9da0742821ed91d
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 20:43:26 +0300

    C#

--- functionloop.cs
+++ functionloop.cs
@@ -11,7 +11,8 @@ namespace Func
 
         static void SemesterGPA()
         {
-            int totalMarks=0;
+            int totalMarks = 0;
+
             Console.WriteLine("Press Enter to Enter Details or any other key to quit");
 
             for (int i = 1; i <= 9; i++)
@@ -24,36 +25,41 @@ namespace Func
             Double Average = totalMarks / 9.0;
             Double GPA = Average / 2;
 
-
-            Double Average = total / 9;
-            Double GPA =Average/2;
-
             string grade = "";
-            if(Average <= 30 && Average >0){
-                Console.WriteLine("D");
+            if (Average <= 30 && Average > 0)
+            {
+                grade = "D";
             }
-            else if(Average<=40 && Average>= 31){
-            Console.WriteLine("C");
+            else if (Average <= 40 && Average >= 31)
+            {
+                grade = "C";
             }
-             else if(Average<=55 && Average>= 41){
-            Console.WriteLine("B-");
+            else if (Average <= 55 && Average >= 41)
+            {
+                grade = "B-";
             }
-             else if(Average<=65 && Average>= 56){
-            Console.WriteLine("B");
+            else if (Average <= 65 && Average >= 56)
+            {
+                grade = "B";
             }
-             else if(Average<=74 && Average>= 66){
-            Console.WriteLine("B+");
+            else if (Average <= 74 && Average >= 66)
+            {
+                grade = "B+";
             }
-             else if(Average<=79 && Average>= 75){
-            Console.WriteLine("A-");
+            else if (Average <= 79 && Average >= 75)
+            {
+                grade = "A-";
             }
-             else if(Average<=99 && Average>= 80){
-            Console.WriteLine("A");
+            else if (Average <= 99 && Average >= 80)
+            {
+                grade = "A";
             }
             else
-            Console.WriteLine("Invalid value..Not Allowed");
+            {
+                grade = "Invalid value..Not Allowed";
+            }
 
-            Console.WriteLine($"Total Marks: {total}, Average: {Average}, GPA: {GPA}, grade:{grade}");
+            Console.WriteLine($"Total Marks: {totalMarks}, Average: {Average}, GPA: {GPA}, grade: {grade}");
         }
 
         public static void Main()
