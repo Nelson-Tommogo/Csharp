c35edf7f1dc0a44b44a314f955e294fa41f2af1e
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 17:03:53 +0300

    C#

--- fucntion.cs
+++ fucntion.cs
@@ -5,27 +5,38 @@ public class Functions{
         Console.WriteLine("Let's Learn Something new today");
     }
     static void SemesterGPA(){
+        Console.WriteLine("Press 1 to Enter Details and  Enter to quit");
+
         int unit1 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit1");
+
         int unit2 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit2");
+
         int unit3 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit3");
-        int unit3 = int.Parse(Console.ReadLine());
+
+        int Unit4 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit4");
-        int unit4 = int.Parse(Console.ReadLine());
+
+        int Unit5 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit5");
-        int unit5 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Marks of Unit6");
+
         int Unit6 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter Marks of Unit7");
+        Console.WriteLine("Enter Marks of Unit6");
+
         int unit7 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit7");
+
+        int Unit8 = int.Parse(Console.ReadLine());
         Console.WriteLine("Enter Marks of Unit8");
-        int unit8 = int.Parse(Console.ReadLine());
+
+        int unit9 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter Marks of Unit9");
     }
     public static void Main(){
         Console.WriteLine("This is the Function");
-        greeting();
         SemesterGPA();
+        greeting();
     }    
 }}
