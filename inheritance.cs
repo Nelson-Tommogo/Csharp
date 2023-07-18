d613320bb8f4b3b7bc2f69468bc552aa98861064
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-18 20:52:58 +0300

    C#

--- inheritance.cs
+++ inheritance.cs
@@ -1,6 +1,12 @@
 using System;
 namespace inherit{
-    public class inherit{
+    public class animals{
+        public string name="animals";
+        public string breath="oxygen";
+        public bool respiration= true
+
+    }
+    public class human:animals{
         
     }
 }
